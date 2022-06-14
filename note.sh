8b526581fe521682bdb6541f5d2ff64b2e7af949
Bewketu Tadilo <bewketu@yandex.com>
2022-06-14 18:36:24 +0300

    Update note.sh

--- note.sh
+++ note.sh
@@ -1,9 +1,9 @@
 #with folder move mkasftp to #Download folder. for terminone.
 #run: sh note.sh
 mkdir ~/bin;
-print "PATH=$PATH:~/bin; export PATH; cd /sdcard/Download;" > ~/.shrc;
+echo "PATH=$PATH:~/bin; export PATH; cd /sdcard/Download;" > ~/.shrc;
 
 cp /sdcard/Download/mkasftp ~/bin;
 chmod a+x ~/bin/mkasftp
 
-print "finished...now run mkasftp on the terminal.";
+echo "finished...now run mkasftp on the terminal.";
